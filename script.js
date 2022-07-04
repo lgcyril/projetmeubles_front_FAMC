80ecfdf92d450ed96d9142ee052d0de1ebf8b452
Cyril <lgcyriltt@gmail.com>
2022-07-04 17:12:15 +0200

    Test API

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // Création fonction bdAdultes
-async function bdAdultes() {
+async function getAPI() {
     var myHeaders = new Headers();
     var myInit = {
         method: 'GET',
         headers: myHeaders,
-        mode: 'cors',
-        cache: 'default'
+        // mode: 'cors',
+        // cache: 'default'
     };
     try {
-        let response = await fetch('https://opendata.paris.fr/api/records/1.0/search/?dataset=les-1000-titres-les-plus-reserves-dans-les-bibliotheques-de-pret&q=&rows=133&sort=-rang&facet=type_de_document&facet=auteur&refine.type_de_document=Bande+dessin%C3%A9e+adulte')
+        let response = await fetch('http://localhost:4000/items')
         let result = await response.json()
-        let rang, titre, auteur;
-        // document.getElementById("afficheTableau").innerHTML="<tr> <th scope='col'>Rang</th> <th scope='col'>Titre</th> <th scope='col'>Auteur</th></tr> <tr><th scope='row' id='rang'> </th> <td id='titre'></td> <td id='auteur'></td> </tr>" ;
+        let rang, name, auteur;
         // AFFICHE LE TABLEAU
-        for (let i = 0; i < 3; i++) {
+        // for (let i = 0; i < 3; i++) {
             //console.log(result.records[i].fields.rang + ". " + result.records[i].fields.titre + " de " + result.records[i].fields.auteur)
             // rang += `<td>` + result.records[i].fields.rang + `<br>` + `</td>`
-            titre += `<td>` + result.records[i].fields.titre + `<br>` + `</td>`
+            //name = result.item.name;
+            console.log("Dans API : " + result) 
             // auteur += `<td>` + result.records[i].fields.auteur + `<br>` + `</td>`
-        }  // fin for
+        // }  // fin for
         // document.getElementById("rang").innerHTML = rang
-        document.getElementById("titre").innerHTML = titre
+       // document.getElementById("titre").innerHTML = name
         // document.getElementById("auteur").innerHTML = auteur
     }  // fin try
     catch (error) {
@@ -28,4 +28,4 @@ async function bdAdultes() {
     }
 }
 
-bdAdultes();
+getAPI();
